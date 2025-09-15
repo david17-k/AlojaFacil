dd241138c69e4f00bbb6588fec8a6a8c9f38c7af
Juan David Castillo Hernandez <juand.castilloh@uqvirtual.edu.co>
2025-09-14 20:25:09 -0500

    mi primer commit

---
+++ src/main/java/com/UQ/AlojaFacil/AlojaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.UQ.AlojaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlojaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlojaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UQ/AlojaFacil/AlojaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UQ.AlojaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlojaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
